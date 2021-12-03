d2599836c5f81af472c084f04e73f312f3b6727a
Savioli Pri <stobbeglaucya@gmail.com>
2021-12-03 19:01:03 -0300

    Resolução exercício Tabuada

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tabuadaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicador, x, numero; 
+
+          Console.Write("Digite um número para exibir a tabuada: ");
+          numero = Int32.Parse(Console.ReadLine());
+          
+          for (x = 1; x<= 10; x ++)
+          {
+                multiplicador = numero*x;
+                Console.WriteLine(numero+"x" + x + "=" + multiplicador);
+          }
+          Console.ReadKey();
+        }
+    }
+}
